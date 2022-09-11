649e9d5d8fa63f9ac6355f183b4b30a1aabc71c2
Ibid Athoillah <ibid.athoillah@nusantara.technology.com>
2022-09-11 16:08:54 +0700

    update todo apps api

--- todo-list/nextjs-todo-apps/pages/index.js
+++ todo-list/nextjs-todo-apps/pages/index.js
@@ -1,19 +1,22 @@
 import Head from 'next/head'
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure, VStack } from "@chakra-ui/react"
-import React, { useEffect } from 'react'
+import { Box, Form, Button, Flex, FormControl, FormLabel, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure, VStack } from "@chakra-ui/react"
+import { useRef, useState, useCallback, useEffect } from 'react'
 import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
 import axios from 'axios'
-const URL = process.env.MONAGO_URL || 'https://api.monago.io/huseindra/todo-apps/v1/tasks'
+const URL = process.env.MONAGO_URL || 'https://api.monago.io/huseindra/todoapps/v1/tasks'
 
 
-const Todo = ({ title, desc, ...rest }) => {
+const Todo = ({ title, id, desc, open, del, ...rest }) => {
   return (
-    <Box className='w-full' p={5}  borderWidth='1px' {...rest}>
+    <Box className='w-full' p={5} borderWidth='1px' {...rest}>
       <Flex className='positioning' alignItems='center'>
-        <Heading fontSize='xl'>{title}</Heading>
+        <Text>
+          <Heading fontSize='xl'> {title} </Heading>
+          <Text fontSize="s">{desc}</Text>
+        </Text>
         <div>
-          <Button colorScheme='yellow'><EditIcon/></Button>
-          <Button colorScheme='red'  ml='2'><DeleteIcon/></Button>
+          <Button onClick={() => open({ name: title, _id: id, description: desc, action: "update" })} colorScheme='yellow'><EditIcon /></Button>
+          <Button onClick={() => { return confirm("Are you sure delete " + title + "?") && del(id) }} colorScheme='red' ml='2'><DeleteIcon /></Button>
         </div>
       </Flex>
     </Box>
@@ -23,31 +26,119 @@ const Todo = ({ title, desc, ...rest }) => {
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
-  const initialRef = React.useRef(null)
-  const finalRef = React.useRef(null)
+  const initialRef = useRef(null)
+  const finalRef = useRef(null)
+  const [tasks, setTask] = useState([])
+  const [formTask, setFormTask] = useState({})
+
+  const fetchTodo = useCallback(async () => {
+
+    let result = []
+    try {
+
+      const response = await axios.get(`${URL}?page=1&limit=10&orderby=asc`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      result = response.data.data
+
+      setTask(result)
+
+    } catch (error) {
+      console.log(error.message)
+    }
+    return result
+  }, [tasks])
+
+  const createTodo = useCallback(async () => {
+    try {
+
+      await axios.post(`${URL}`, formTask, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      fetchTodo()
+      setFormTask({})
+      onClose()
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  }, [formTask])
 
 
-  const fetchTodo = async () => {
-  try {
 
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  const updateTodo = useCallback(async () => {
+    try {
+      await axios.put(`${URL}/${formTask._id}`, {
+        name: formTask.name,
+        description: formTask.description
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      fetchTodo()
+      setFormTask({})
+      onClose()
+
+    } catch (error) {
+      console.log(error.message)
     }
 
-     const response = await axios.get(`${URL}/?page=1&limit=10&orderby=asc`, config)
-     console.log(response.json())
-     
-  } catch (error) {
-    console.log(error.message)
+  }, [formTask])
+
+
+  const save = useCallback(async () => {
+
+    console.log(formTask)
+    if (formTask.action == "update") {
+      updateTodo()
+    } else {
+      createTodo()
+    }
+
+  }, [formTask])
+
+
+  const deleteTodo = useCallback(async (id) => {
+
+    try {
+
+      await axios.delete(`${URL}/${id}`, formTask, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      fetchTodo()
+
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }, [formTask])
+
+  const openUpdate = (task) => {
+    setFormTask(task)
+    onOpen()
   }
 
-}
 
-useEffect(() => {
-  fetchTodo()
-},[])
+
+  useEffect(() => {
+    fetchTodo()
+  }, [])
+
+  const updateFormTask = evt => setFormTask({
+    ...formTask,
+    [evt.target.name]: evt.target.value
+  });
+
 
   return (
     <div className='main-background'>
@@ -62,52 +153,59 @@ useEffect(() => {
             <a>Todo</a>
           </div>
           <div>
-            <Button colorScheme='orange' onClick={onOpen}>Add Task</Button>
+            <Button colorScheme='orange' onClick={() => { onOpen(); setFormTask({}) }} >Add Task</Button>
           </div>
         </nav>
         <div className='container'>
-        <Heading fontSize='xl' mb={5}>Todo List</Heading>
-        <VStack spacing={8}>
-          <Todo
-            title='Plan Money'
-          />
-          <Todo
-            title='Save Money'
-          />
-        </VStack>
+          <Heading fontSize='xl' mb={5}>Todo List</Heading>
+          <VStack spacing={8}>
+            {
+              tasks.map(task => {
+                return (
+                  <Todo
+                    title={task.name}
+                    desc={task.description}
+                    id={task._id}
+                    del={deleteTodo}
+                    open={openUpdate}
+                  />
+                )
+              })
+            }
+          </VStack>
         </div>
       </Box>
-        <Modal
-          initialFocusRef={initialRef}
-          finalFocusRef={finalRef}
-          isOpen={isOpen}
-          onClose={onClose}
-        >
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
         <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Activity</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6}>
-              <FormControl>
-                <FormLabel>Name Activity</FormLabel>
-                <Input ref={initialRef} placeholder='Name' />
-              </FormControl>
+        <ModalContent>
+          <ModalHeader>Activity</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Name Activity</FormLabel>
+              <Input value={formTask.name} name="name" ref={initialRef} onChange={updateFormTask} placeholder='Name' />
+            </FormControl>
 
-              <FormControl mt={4}>
-                <FormLabel>Description Activity</FormLabel>
-                <Input placeholder='Description' />
-              </FormControl>
-            </ModalBody>
+            <FormControl mt={4}>
+              <FormLabel>Description Activity</FormLabel>
+              <Input value={formTask.description} name="description" onChange={updateFormTask} placeholder='Description' />
+            </FormControl>
+          </ModalBody>
 
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3}>
-                Save
-              </Button>
-              <Button onClick={onClose}>Cancel</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-    </div>
-    
+          <ModalFooter>
+            <Button onClick={save} colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div >
+
   )
 }
